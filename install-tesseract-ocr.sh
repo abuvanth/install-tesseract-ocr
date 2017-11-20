8ba53d773a1c29a158341b6b7cba67e5fd8d3a9e
syed abuthahir <abuvanth@gmail.com>
2017-11-20 23:19:47 +0530

    Update install-tesseract-ocr.sh

--- install-tesseract-ocr.sh
+++ install-tesseract-ocr.sh
@@ -10,20 +10,9 @@ sudo apt-get install libicu-dev -y
 sudo apt-get install libpango1.0-dev -y
 sudo apt-get install libcairo2-dev -y
 git clone https://github.com/DanBloomberg/leptonica.git
-cd leptonica
-./configure
-make
-sudo make install
-cd ..
+cd leptonica && ./configure && make && sudo make install
 git clone https://github.com/tesseract-ocr/tesseract.git
-cd tesseract
-./autogen.sh
-./configure
-make
-sudo make install
-sudo ldconfig
-make training
-sudo make training-install
+cd tesseract && ./autogen.sh && ./configure && make && sudo make install && sudo ldconfig && make training && sudo make training-install
 wget https://github.com/tesseract-ocr/tessdata/raw/master/tam.traineddata
 wget https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata
 sudo mv tam.traineddata /usr/local/share/
