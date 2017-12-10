8de9451ea5696e4959bbf058a1f140dfa1da7cc5
syed abuthahir <abuvanth@gmail.com>
2017-12-10 11:28:57 +0530

    Update install-tesseract-ocr.sh

--- install-tesseract-ocr.sh
+++ install-tesseract-ocr.sh
@@ -1,21 +1,9 @@
-sudo apt-get install g++ -y
-sudo apt-get install autoconf automake libtool -y
-sudo apt-get install autoconf-archive -y
-sudo apt-get install pkg-config -y
-sudo apt-get install libpng12-dev -y
-sudo apt-get install libjpeg8-dev -y
-sudo apt-get install libtiff5-dev -y
-sudo apt-get install zlib1g-dev -y
-sudo apt-get install libicu-dev -y
-sudo apt-get install libpango1.0-dev -y
-sudo apt-get install libcairo2-dev -y
-git clone https://github.com/DanBloomberg/leptonica.git
-cd leptonica && ./autobuild && ./make-for-auto && ./make-for-local && ./configure && make && sudo make install
-git clone https://github.com/tesseract-ocr/tesseract.git
-cd tesseract && ./autogen.sh && ./configure && make && sudo make install && sudo ldconfig && make training && sudo make training-install
+sudo apt-get update 
+sudo apt-get install -y software-properties-common 
+sudo add-apt-repository -y ppa:alex-p/tesseract-ocr
+sudo apt-get update 
+sudo apt-get install -y tesseract-ocr 
 wget https://github.com/tesseract-ocr/tessdata/raw/master/tam.traineddata
-wget https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata
-sudo mv tam.traineddata /usr/local/share/
-sudo mv eng.traineddata /usr/local/share/
+sudo mv tam.traineddata /usr/share/tesseract-ocr/4.00/
 sudo pip install pyocr
 echo "tesseract-ocr installed successfully"
